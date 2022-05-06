71ccac641140c6f6734fe7db01beecb11fc195b3
Oriques de Souza <104857614+oriques@users.noreply.github.com>
2022-05-06 00:05:20 -0300

    add jumpScale to Jump Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f,0);
-
     public float speed;
     public float speedRun;
-
     public float forceJump = 2;
+
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.5f;
+    public float animationDuration = 0.3f;
+    public Ease ease = Ease.OutBack;
+
 
     private float _currentSpeed;
 
@@ -19,6 +26,7 @@ public class Player : MonoBehaviour
     {
         HandleMoviment();
         HandleJump();
+        
     } 
 
     private void HandleMoviment()
@@ -55,7 +63,17 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.velocity = Vector2.up * forceJump;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+
+            HandleScaleJump();
         }
     }
 
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
 }
