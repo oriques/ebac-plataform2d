85daebee0099aaec8c4d809bb599f37b81b48e95
Oriques de Souza <104857614+oriques@users.noreply.github.com>
2022-05-15 21:00:36 -0300

    Add count coin

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,29 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ecco.Core.Singleton;
+using TMPro;
 
 
 public class ItemManager : Singleton<ItemManager>
 {
     
     public int coins;
+    public TextMeshProUGUI uiTextCoins;
 
 
     private void Start()
     {
-      
-            Reset();
-      
+       Reset();
     }
     private void Reset()
     {
         coins = 0;
-
+        UpdateUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateUI();
     }
 
+    private void UpdateUI()
+    {
+        uiTextCoins.text = coins.ToString();
+    }
 }
