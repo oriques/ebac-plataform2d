1ee13fef076c349d6f65694a19190233ea6a6cef
Oriques de Souza <104857614+oriques@users.noreply.github.com>
2022-05-26 23:53:11 -0300

    Add Jump VFX Astronaut

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -17,6 +17,12 @@ public class Player : MonoBehaviour
 
     private float _currentSpeed;
 
+    [Header("Jump Collision Check")]
+    public Collider2D collider2D;
+    public float distToGround;
+    public float spaceToGround = .1f;
+    public ParticleSystem jumpVFX;
+
 
 
     private void Awake()
@@ -29,8 +35,21 @@ public class Player : MonoBehaviour
 
         _currentPlayer = Instantiate(soPlayerSetup.player, transform);
 
+    if( collider2D != null)
+        {
+            distToGround = collider2D.bounds.extents.y;
+        }
+
 
     }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + spaceToGround);
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
+        
+    }
+
 
     private void OnPlayerKill()
     {
@@ -43,6 +62,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        IsGrounded();
         HandleMoviment();
         HandleJump();
         
@@ -102,14 +122,24 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
            
             myRigidbody.velocity = Vector2.up * soPlayerSetup.forceJump;
             DOTween.Kill(myRigidbody.transform);
 
+            PlayJumpVFX();
+         
+
         }
     }
+    private void PlayJumpVFX()
+    {
+        if (jumpVFX != null) jumpVFX.Play();
+    }
+
+  
+
 
     public void DestroyMe()
     {
